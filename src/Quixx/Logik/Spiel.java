114b5ae46ec142f52b771eec47558227d7b83fbb
ceyh61 <x61ceyhun61x@gmail.com>
2023-03-06 19:45:33 +0100

    test

---
+++ src/Quixx/Logik/Spiel.java
@@ -0,0 +1,8 @@
+package src.Quixx.Logik;
+
+public class Spiel {
+    public static void main(String[] args){
+        System.out.println("Hallo Welt");
+
+    }
+}
--- src/Quixx/Logik/Spielerkarte.java
+++ src/Quixx/Logik/Spielerkarte.java
@@ -1,4 +1,4 @@
-package src;
+package src.Quixx.Logik;
 
 public class Spielerkarte {
 }
